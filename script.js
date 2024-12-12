e4bd163f4115524e55f4a83cd39ebc77a884adff
aniquatasnim <aniqua177@gmail.com>
2024-12-12 23:23:13 +0530

    removing update and adding delete functionality

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 
 let ctr=0;
-function updateToInprogress(index){
-    let state = "Inprogress";
-    console.log("Updating to in Progress called for"+index)  
-    const element = document.getElementById(index)
-    console.log(element)
-    element.setAttribute("state", state)
-    document.querySelector(".inprogress").appendChild(element);
+function deleteTodo(index){
+    console.log("executing delete for index"+index)
+    
+    const deleteElement = document.getElementById(index)
+    console.log(deleteElement);
+    document.querySelector(".todo").removeChild(deleteElement)
+    
 
 }
-
-
-
 function addTodo() {
     
-let state = "start";
 const inputElement= document.querySelector("input");
 const value=inputElement.value;
 console.log(value);
-
-const newDivElement = document.createElement("div");
+ if (!value){
+    alert("Please add a task");
+    return;
+ }
+const newDivElement = document.createElement("li");
 newDivElement.setAttribute("id", ctr);
-newDivElement.innerHTML= value +"   <button onclick=updateToInprogress("+ctr+")>"+state+"</button>"
+newDivElement.innerHTML= value+"  <button onclick= deleteTodo("+ctr+")>Delete</button>"
  ctr = ctr + 1;
- 
 document.querySelector(".todo").appendChild(newDivElement)
 }
